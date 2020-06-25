96cce9a52c3e53316f634e58c836c2b84c9a5ca1
salvadorc94 <secg.1994@gmail.com>
2020-06-24 18:53:57 -0600

    Finished "Show Model", effects, resources and controller.

--- src/sample/controller/ModelController.java
+++ src/sample/controller/ModelController.java
@@ -6,11 +6,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import javafx.scene.input.MouseEvent;
+
 
 import java.io.IOException;
 
-public class ModelController {
+public class ModelController{
+
+    @FXML
+    private ImageView questionImage;
+    private Image question = new Image(getClass().getResource("../resources/QuestionMark.png").toExternalForm());
+    private Image modelo = new Image(getClass().getResource("../resources/modelo.png").toExternalForm());
+
     @FXML
     private void returnMenu(ActionEvent event) throws IOException {
         Parent domain = FXMLLoader.load(getClass().getResource("../ui/menu.fxml"));
@@ -20,4 +31,18 @@ public class ModelController {
         window.setScene(newScene);
         window.show();
     }
+
+    @FXML
+    private void onMouseEnter(MouseEvent event){
+        questionImage.setImage(modelo);
+        questionImage.setFitHeight(800);
+        questionImage.setFitWidth(400);
+    }
+    @FXML
+    private void onMouseExit(MouseEvent event){
+        questionImage.setImage(question);
+        questionImage.setFitHeight(124);
+        questionImage.setFitWidth(137);
+    }
+
 }
